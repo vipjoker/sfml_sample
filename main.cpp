90d2f692329df06f81f60c0124f834cff23b6520
omakhobei <tajcig@ya.ru>
2016-10-31 17:35:28 +0200

    initial files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main (){
+	sf::RenderWindow window (sf::VideoMode(480,640),"SFML works");
+	sf::CircleShape shape(200.0f);
+	shape.setFillColor(sf::Color::Green);
+
+	while(window.isOpen()){
+		sf::Event event;
+		while(window.pollEvent(event)){
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+	return 0;
+} 
